0e9edd68241962129713812d23dc69fb9aa2c9fe
Vince Weaver <vince@deater.net>
2014-01-05 00:32:47 -0500

    Add test for invalid 256 color mode

---
+++ example_ansi/code/256color_oflo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+	int x;
+
+	printf("\x1b[0;37mTesting overflowing 256 color mode:\n");
+
+	for(x=0;x<1024;x++) {
+		if (x==16) printf("\n");
+		if ((x>16) && ((x-16)%48==0)) printf("\n");
+		printf("\x1b[38;5;%dm#",x);
+	}
+	printf("\x1b[1;37;40m\n");
+	return 0;
+}
