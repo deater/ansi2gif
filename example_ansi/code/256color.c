3e1a68037f9fdc2532876090d31f94dc82fe3706
Vince Weaver <vince@deater.net>
2014-01-05 00:54:06 -0500

    Update 256 color test to also do background

--- example_ansi/code/256color.c
+++ example_ansi/code/256color.c
@@ -8,9 +8,17 @@ int main(int argc, char **argv) {
 
 	for(x=0;x<256;x++) {
 		if (x==16) printf("\n");
-		if ((x>16) && ((x-16)%12==0)) printf("\n");
+		if ((x>16) && ((x-16)%48==0)) printf("\n");
 		printf("\x1b[38;5;%dm#",x);
 	}
+	printf("\n");
+
+	for(x=0;x<256;x++) {
+		if (x==16) printf("\n");
+		if ((x>16) && ((x-16)%48==0)) printf("\n");
+		printf("\x1b[48;5;%dm#",x);
+	}
+
 	printf("\x1b[1;37;40m\n");
 	return 0;
 }
